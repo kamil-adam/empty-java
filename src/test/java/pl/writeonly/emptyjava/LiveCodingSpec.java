905af24a19df1e5f1549f2311e21275b469c92db
Kamil Adam <kamil.adam.zabinski@gmail.com>
2023-12-12 21:28:35 +0100

    Update src

--- src/test/java/pl/writeonly/emptyjava/LiveCodingSpec.java
+++ src/test/java/pl/writeonly/emptyjava/LiveCodingSpec.java
@@ -1,11 +1,13 @@
 package pl.writeonly.emptyjava;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LiveCodingSpec {
 
+    @DisplayName("Add")
     @Test
     public void addSpec() {
         var result = LiveCoding.add(1, 2);
